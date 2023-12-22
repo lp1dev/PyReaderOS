0eb02ff51afc43e85c573ca442e72b215902178f
Jeremie A. <mail@lp1.eu>
2023-12-22 21:30:48 +0100

    separated in two windows

--- main.py
+++ main.py
@@ -10,6 +10,7 @@ from ui.panels.files import Files
 from ui.panels.settings import Settings
 
 SCALING=2
+TAB_SIZE = 100
 CURRENT_PANEL = None
 
 font = ("Arial", 18)
@@ -20,18 +21,19 @@ sg.set_options(scaling=SCALING)
 root = tk.Tk()
 width = root.winfo_screenwidth()
 height = root.winfo_screenheight()
+root.withdraw()
 
 header = Header()
 home = Home()
 files = Files()
 settings = Settings()
 
-def init_window():
-    window = sg.Window("Bar", [ footer() ], no_titlebar=True, location=(0, 0), keep_on_top=True, size=(width, 100), element_justification="c").Finalize()
-#    window = sg.Window('PyReaderOS', [header.build(), settings.build(), files.build(), home.build(), footer()], no_titlebar=True, location=(0,0), size=(width,height), keep_on_top=False, font=font, element_justification="c").Finalize()
-#    home.show()
+def init_windows():
+    tab = sg.Window("Bar", [ footer() ], no_titlebar=True, location=(0, height - TAB_SIZE), keep_on_top=True, size=(width, TAB_SIZE), element_justification="c").Finalize()
+    window = sg.Window('PyReaderOS', [header.build(), settings.build(), files.build(), home.build() ], no_titlebar=True, location=(0,0), size=(width,height - tab.size[1]), keep_on_top=False, font=font, element_justification="c").Finalize()
+    home.show()
     window.TKroot["cursor"] = "none"
-    return window
+    return tab, window
 
 def switch_panel(panel):
     global CURRENT_PANEL
@@ -41,16 +43,15 @@ def switch_panel(panel):
         CURRENT_PANEL.show()
 
 
-window = init_window()
+tab, window = init_windows()
 
 CURRENT_PANEL = home
 
-#header.update()
+header.update()
 
 while True:
     refresh = False
-    event, values = window.read()
-
+    window, event, values = sg.read_all_windows()
     print("Event", event)
 
     if event and event.startswith("ui-panel-files-"):
@@ -61,16 +62,12 @@ while True:
     
     if event == "ui-panel-home-files":
         switch_panel(files)
-#        window.refresh()
         
     if event == "ui-panel-home-settings":
         switch_panel(settings)
-#        window.refresh()
-
 
     elif event == "ui-footer-home":
         switch_panel(home)
-#        window.refresh()
 
     elif event == "ui-panel-settings-brightness-warm":
         set_brightness(int(values['ui-panel-settings-brightness-warm']), "warm")

033b62acfbe555b2cf174d9dbffeadd3532098fc
Jeremie A. <mail@lp1.eu>
2023-12-22 21:12:48 +0100

    added main and packages file

--- main.py
+++ main.py
@@ -20,11 +20,16 @@ sg.set_options(scaling=SCALING)
 root = tk.Tk()
 width = root.winfo_screenwidth()
 height = root.winfo_screenheight()
+
 header = Header()
+home = Home()
+files = Files()
+settings = Settings()
 
 def init_window():
-    window = sg.Window('PyReaderOS', [header.build(), settings.build(),  files.build(), home.build(), footer()], no_titlebar=True, location=(0,0), size=(width,height), keep_on_top=True, font=font, element_justification="c").Finalize()
-    home.show()
+    window = sg.Window("Bar", [ footer() ], no_titlebar=True, location=(0, 0), keep_on_top=True, size=(width, 100), element_justification="c").Finalize()
+#    window = sg.Window('PyReaderOS', [header.build(), settings.build(), files.build(), home.build(), footer()], no_titlebar=True, location=(0,0), size=(width,height), keep_on_top=False, font=font, element_justification="c").Finalize()
+#    home.show()
     window.TKroot["cursor"] = "none"
     return window
 
@@ -35,15 +40,12 @@ def switch_panel(panel):
         CURRENT_PANEL = panel
         CURRENT_PANEL.show()
 
-home = Home()
-files = Files()
-settings = Settings()
 
 window = init_window()
 
 CURRENT_PANEL = home
 
-header.update()
+#header.update()
 
 while True:
     refresh = False
@@ -59,16 +61,16 @@ while True:
     
     if event == "ui-panel-home-files":
         switch_panel(files)
-        window.refresh()
+#        window.refresh()
         
     if event == "ui-panel-home-settings":
         switch_panel(settings)
-        window.refresh()
+#        window.refresh()
 
 
     elif event == "ui-footer-home":
         switch_panel(home)
-        window.refresh()
+#        window.refresh()
 
     elif event == "ui-panel-settings-brightness-warm":
         set_brightness(int(values['ui-panel-settings-brightness-warm']), "warm")

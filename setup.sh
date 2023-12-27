8af78166cb897f3a9df017df9b1cefafc343ad0a
Jeremie A. <mail@lp1.eu>
2023-12-27 20:48:34 +0100

    added suspend feature with the button

--- setup.sh
+++ setup.sh
@@ -1,2 +1,2 @@
-echo "$USER   ALL=NOPASSWD: /usr/sbin/pm-suspend" >> /etc/sudoers.d/suspend
+echo "$USER   ALL=NOPASSWD: /usr/bin/suspend.sh" >> /etc/sudoers.d/suspend
 cp suspend.sh /usr/bin/suspend.sh
--- suspend.sh
+++ suspend.sh
@@ -1,25 +1,25 @@
 #!/bin/sh
 
+DISPLAY=:0
 FILE="$HOME/.asleep"
 
 if [ -f "$FILE" ];then
-    notify-send "Waking up"
-    rm $FILE
+    killall feh
     echo "1" > /sys/class/leds/e60k02:white:on/brightness
     brightnessctl -d "backlight_warm" s $(sed -n 0p $FILE)
     brightnessctl -d "backlight_cold" s $(sed -n 1p $FILE)
-    echo 1-0024 | sudo tee /sys/bus/i2c/drivers/cyttsp5/unbind
+    echo 1-0024 | tee /sys/bus/i2c/drivers/cyttsp5/unbind
     sleep 1
-    echo 1-0024 | sudo tee /sys/bus/i2c/drivers/cyttsp5/bind
+    echo 1-0024 | tee /sys/bus/i2c/drivers/cyttsp5/bind
+    rm $FILE
 else
-    notify-send "Suspending..."
+    feh -Y -x -q -D5 -B white -F -Z -z -r /home/lp1/Pictures/&
     touch $FILE
-    echo "0" > /sys/class/leds/e60k02:white:on/brightness
-    brightnessctl -d "backlight_warm" g > $FILE
-    brightnessctl -d "backlight_cold" g > $FILE
+    brightnessctl -d "backlight_warm" g >> $FILE
+    brightnessctl -d "backlight_cold" g >> $FILE
     brightnessctl -d "backlight_warm" s 0
     brightnessctl -d "backlight_cold" s 0
-    
-    sleep 1
-    sudo pm-suspend
+    echo "0" > /sys/class/leds/e60k02:white:on/brightness
+    sleep 5
+    pm-suspend
 fi
